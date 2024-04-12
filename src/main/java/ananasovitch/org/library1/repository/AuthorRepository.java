a2aabcb0b0beeee963d93a5577e4abd01b549267
Aleksey Vetrov <vetrovalexey28@gmail.com>
2024-04-13 02:35:15 +0300

    обновил структуру проетка

--- src/main/java/ananasovitch/org/library1/repository/AuthorRepository.java
+++
@@ -1,9 +0,0 @@
-package ananasovitch.org.library1.repository;
-
-import ananasovitch.org.library1.entity.AuthorEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<AuthorEntity, Long> {
-    boolean existsByFirstNameAndFamilyName(String firstName, String familyName);
-}
-
--- src/main/java/ananasovitch/org/library1/service/ResponseChecker.java
+++
@@ -1,9 +0,0 @@
-package ananasovitch.org.library1.service;
-
-import org.junit.jupiter.api.Assertions;
-
-public class ResponseChecker {
-    public void checkResponse(Object expected, Object actual) {
-        Assertions.assertEquals(expected, actual);
-    }
-}
