9c9b796a5be69f8b4347775c54c2f85cff6836ea
Aleksey Vetrov <vetrovalexey28@gmail.com>
2024-04-18 21:13:14 +0300

    dd new files for library1

---
+++ src/main/java/ananasovitch/org/library1/model/Author.java
@@ -0,0 +1,28 @@
+package ananasovitch.org.library1.model;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+
+@Data
+@NoArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@XmlRootElement(name = "author")
+public class Author {
+    @XmlElement(name = "id")
+    private long id;
+
+    @XmlElement(name = "firstName")
+    private String firstName;
+
+    @XmlElement(name = "secondName")
+    private String secondName;
+
+    public Author(long id, String firstName, String secondName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.secondName = secondName;
+    }
+}
---
+++ src/test/java/ananasovitch/org/library1/service/RestAssuredClientTest.java
@@ -0,0 +1,55 @@
+package ananasovitch.org.library1.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import ananasovitch.org.library1.model.Author;
+import ananasovitch.org.library1.model.Book;
+import ananasovitch.org.library1.model.BookRequest;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Story;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Epic("Library Service Tests")
+public class RestAssuredClientTest {
+    private static RestAssuredClient client;
+
+    @BeforeAll
+    public static void setup() {
+        client = new RestAssuredClient("http://localhost:8080");
+    }
+
+    @Test
+    @Story("Get All Books for Author")
+    @DisplayName("Get All Books for Author Successful Request")
+    public void testGetAllBooksForAuthor() {
+        Long authorId = 1L;
+        List <Book> bookList = client.getAuthorBooks(authorId);
+        assertFalse(bookList.isEmpty());
+    }
+
+    @Test
+    @Story("Save New Book")
+    @DisplayName("Save New Book Successful Request")
+    public void testSaveNewBook() {
+        BookRequest bookRequest = new BookRequest();
+        bookRequest.setBookTitle("New Book");
+        bookRequest.setAuthor(new Author(1L, "First", "Second"));
+        Book savedBook = client.saveNewBook(bookRequest);
+        assertEquals(bookRequest.getBookTitle(), savedBook.getBookTitle());
+    }
+
+    @Test
+    @Story("Get All Books for Author XML")
+    @DisplayName("Get All Books for Author XML Successful Request")
+    public void testGetAllBooksForAuthorXml() {
+        Long authorId = 1L;
+        List<Book> books = client.getAuthorBooksXml(authorId);
+        assertFalse(books.isEmpty());
+    }
+}
